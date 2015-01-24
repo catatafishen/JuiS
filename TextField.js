341c08a381f2fd39879223c1c79bd5a031113cb3
Catatafish <henrik@westergard.org>
2015-01-25 00:38:11 +0200

    Added new properties to TextField

--- TextField.js
+++ TextField.js
@@ -17,8 +17,20 @@ JuiS.TextField = function (callback) {
     this.addProperty("lineHeight", "nodeStyle");
     this.addProperty("textShadow", "nodeStyle");
     this.addProperty("textOverflow", "nodeStyle", "ellipsis");
+    this.addProperty("hideContent", function(value) {
+        if (value === true) {
+            this.node.type = "password";
+        } else {
+            this.node.type = "text";
+        }
+    }, "");
+    
     this.addProperty("value", function(value) {
         this.node.value = value;
+    }, "");
+    
+    this.addProperty("placeholder", function(value) {
+        this.node.placeholder = value;
     }, "");
     
     //Common methods
