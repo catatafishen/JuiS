4b70b090245eb60f06aa8497bbc37b2ab6eed0e8
Catatafish <henrik@westergard.org>
2015-01-28 23:13:18 +0200

    Created JuiS baseObject
    It is listenable, has references to body and document, and it relays
    events from body.

---
+++ JuiS.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log(JuiS);
+    var oldJuiS = JuiS;
+    JuiS = new (function () {
+        Object.keys(oldJuiS).forEach(function (key) {
+            this[key] = oldJuiS[key];
+        }, this);
+        this.initElement(document.body);
+        this.window = window;
+        this.createDOMEventRelay("onscroll", "scroll");
+    }.addMixin(oldJuiS.ElementMixin))();
+    
+    JuiS.document = new (function () {
+        this.initElement(document.documentElement);
+        this.nextListenable = JuiS;
+    }.addMixin(oldJuiS.ElementMixin))();
+});
